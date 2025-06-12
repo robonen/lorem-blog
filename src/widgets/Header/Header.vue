be9d8ef1f44d030cfcc9a5c84339f7e1546a7804
robonen <robonenandrew@gmail.com>
2025-06-13 00:11:28 +0700

    feat(ui): add Logo and Header components

---
+++ src/widgets/Header/Header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Logo } from '@/shared/icons';
+import { NavigationMenu, type NavigationMenuItem } from '../NavigationMenu';
+
+const items = [
+  { name: 'Главная', path: '/' },
+  { name: 'Блог', path: '/blog' },
+] satisfies NavigationMenuItem[];
+</script>
+
+<template>
+  <header class="p-4 dark bg-background text-foreground">
+    <div class="mx-auto container flex space-x-18">
+      <Logo class="h-6" aria-label="Logo" />
+      <NavigationMenu :items />
+    </div>
+  </header>
+</template>
