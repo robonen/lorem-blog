7eb1528a55e75a7981f9d84038fcf6dd4b1aae00
robonen <robonenandrew@gmail.com>
2025-06-15 15:39:33 +0700

    feat(comment): add Comment entity with types and UI component

---
+++ src/entities/Comment/types.ts
@@ -0,0 +1,7 @@
+export interface PostComment {
+  id: number;
+  avatar: string;
+  name: string;
+  text: string;
+  created_at: string;
+}
---
+++ src/entities/Comment/ui/Comment.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+export interface CommentProps extends Omit<PostComment, 'text'> {}
+</script>
+
+<script setup lang="ts">
+import type { PostComment } from '../types';
+import { formatDateTime } from '@/shared/utils';
+
+const props = defineProps<CommentProps>();
+</script>
+
+<template>
+  <div class="flex items-center gap-3">
+    <img
+      class="w-10 h-10 rounded-full object-cover"
+      :src="props.avatar"
+      :alt="props.name"
+    >
+    <div class="flex flex-col">
+      <span class="text-sm font-semibold text-gray-900">
+        {{ props.name }}</span>
+      <p class="text-sm">
+        <slot />
+      </p>
+      <span class="text-xs text-foreground-muted-light">
+        {{ formatDateTime(props.created_at) }}</span>
+    </div>
+  </div>
+</template>
