42b6c90c1a32c67de1e7bdbbed77e22c6050bcc8
robonen <robonenandrew@gmail.com>
2025-07-14 02:48:20 +0700

    chore(lint): disable import/first rule for vue sfc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,28 @@
 import antfu from '@antfu/eslint-config';
 
-export default antfu({
-  stylistic: {
-    indent: 2,
-    semi: true,
-    quotes: 'single',
-    overrides: {
-      'style/comma-dangle': ['error', 'always-multiline'],
+export default antfu(
+  {
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: 'single',
+      overrides: {
+        'style/comma-dangle': ['error', 'always-multiline'],
+      },
+    },
+    vue: true,
+    typescript: true,
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+      'regexp/no-obscure-range': ['error', {
+        allowed: 'all',
+      }],
     },
   },
-  rules: {
-    'unused-imports/no-unused-imports': 'error',
-    'regexp/no-obscure-range': ['error', {
-      allowed: 'all',
-    }],
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'import/first': 'off',
+    },
   },
-  vue: true,
-  typescript: true,
-});
+);
